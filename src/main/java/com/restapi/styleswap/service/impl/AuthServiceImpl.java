b05e336d45f391e7cf8c2152a4bcf0e35e7e0038
Karol Mac <karol200242@gmail.com>
2025-01-01 19:50:07 +0100

    refactor in auth service + connecting stripe :)
    When user is registered, he get's link to register on stripe payment system
    
    Next:
     - dodaj koknta stripe dla encji z pliku dataToLoad.sql - żeby sprawdzić inne scenariusze
     - sprawdź co się dzieje dla złych danych
     - koknto zapisuje ID oknta stripe, nawet jeśli nie jest ono potwierdzone - lepiej je zapisywać dopiero po webhook-u

--- src/main/java/com/restapi/styleswap/service/impl/AuthServiceImpl.java
+++ src/main/java/com/restapi/styleswap/service/impl/AuthServiceImpl.java
@@ -2,7 +2,6 @@ package com.restapi.styleswap.service.impl;
 
 import com.restapi.styleswap.entity.Role;
 import com.restapi.styleswap.entity.User;
-import com.restapi.styleswap.exception.ApiException;
 import com.restapi.styleswap.payload.JwtAuthResponse;
 import com.restapi.styleswap.payload.LoginDto;
 import com.restapi.styleswap.payload.RegisterDto;
@@ -10,10 +9,11 @@ import com.restapi.styleswap.repository.RoleRepository;
 import com.restapi.styleswap.repository.UserRepository;
 import com.restapi.styleswap.security.JwtTokenProvider;
 import com.restapi.styleswap.service.AuthService;
+import com.restapi.styleswap.utils.StripeManager;
 import com.restapi.styleswap.utils.UserUtils;
-import org.springframework.http.HttpStatus;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Account;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -46,49 +46,55 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public JwtAuthResponse login(LoginDto loginDto){
-        try {
-            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                    loginDto.getUsernameOrEmail(), loginDto.getPassword()));
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        Authentication authentication = authenticateUser(loginDto);
+        User user = userRepository.findByUsernameOrEmail(
+                                        loginDto.getUsernameOrEmail(),
+                                        loginDto.getUsernameOrEmail()).get();
 
-            User user = userRepository.findByUsernameOrEmail(loginDto.getUsernameOrEmail(),
-                                                            loginDto.getUsernameOrEmail()).get();
-
-            return createAuthResponse(user, authentication);
-
-        } catch (BadCredentialsException exception){
-            throw new ApiException(HttpStatus.FORBIDDEN, "Wrong username/email or password");
-        }
-    }
-
-    private JwtAuthResponse createAuthResponse(User user, Authentication authentication) {
-        JwtAuthResponse authResponse = new JwtAuthResponse();
-        authResponse.setAccessToken(jwtTokenProvider.generateToken(authentication));
-        var roles = user.getRoles().stream().map(Role::getName).toList();
-        authResponse.setRole(roles.toString());
-        authResponse.setUserId(user.getId());
-        authResponse.setUsernameOrEmail(authentication.getName());
-
-        return authResponse;
+        return createAuthResponse(user, authentication);
     }
 
     @Override
     @Transactional
-    public String register(RegisterDto registerDto) {
+    public String register(RegisterDto registerDto) throws StripeException {
 
         userUtils.validateUsernameAndEmail(registerDto);
         Role userRole = roleRepository.findByName("ROLE_USER").get();
 
+        Account stripeAccount = StripeManager.createStripeAccount(registerDto.getEmail());
+        String stripeRegisterLink = StripeManager.generateStripeRegisterLink(stripeAccount);
+
+        saveUserEntity(registerDto, stripeAccount, userRole);
+
+        return stripeRegisterLink;
+    }
+
+    private void saveUserEntity(RegisterDto registerDto, Account stripeAccount, Role userRole) {
         User user = User.builder()
-                        .email(registerDto.getEmail())
-                        .password(passwordEncoder.encode(registerDto.getPassword()))
-                        .name(registerDto.getName())
-                        .username(registerDto.getUsername())
-                        .roles(Set.of(userRole))
-                        .build();
+                .email(registerDto.getEmail())
+                .password(passwordEncoder.encode(registerDto.getPassword()))
+                .name(registerDto.getName())
+                .username(registerDto.getUsername())
+                .stripeAccountId(stripeAccount.getId())
+                .roles(Set.of(userRole))
+                .build();
 
         userRepository.save(user);
+    }
 
-        return "User sign up successfully";
+    private Authentication authenticateUser(LoginDto loginDto) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginDto.getUsernameOrEmail(), loginDto.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return authentication;
+    }
+
+    private JwtAuthResponse createAuthResponse(User user, Authentication authentication) {
+        return JwtAuthResponse.builder()
+                .accessToken(jwtTokenProvider.generateToken(authentication))
+                .role(user.getRoles().stream().map(Role::getName).toList().toString())
+                .userId(user.getId())
+                .usernameOrEmail(authentication.getName())
+                .build();
     }
 }
