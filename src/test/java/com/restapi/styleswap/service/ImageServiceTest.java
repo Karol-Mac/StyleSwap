5c2d43a9a1ff9d3e48ca621662f934a4273ccce1
Karol Mac <karol200242@gmail.com>
2025-03-01 15:50:08 +0100

    unit test's for ImageService created

--- src/test/java/com/restapi/styleswap/service/ImageServiceTest.java
+++ src/test/java/com/restapi/styleswap/service/ImageServiceTest.java
@@ -6,20 +6,19 @@ import com.restapi.styleswap.exception.ResourceNotFoundException;
 import com.restapi.styleswap.service.impl.ImageServiceImpl;
 import com.restapi.styleswap.utils.ClotheUtils;
 import com.restapi.styleswap.utils.Constant;
+import com.restapi.styleswap.utils.managers.FileManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,6 +33,9 @@ public class ImageServiceTest {
 
     @Mock
     private ClotheUtils clotheUtils;
+
+    @Mock
+    private FileManager fileManager;
 
     @InjectMocks
     private ImageServiceImpl imageService;
@@ -50,95 +52,99 @@ public class ImageServiceTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        ReflectionTestUtils.setField(imageService, "imageDirectory", "test-images");
     }
-
-//    @Test
-//    void saveImage_savesImageSuccessfully() {
-//        MultipartFile file = mock(MultipartFile.class);
-//        when(file.isEmpty()).thenReturn(false);
-//        when(file.getOriginalFilename()).thenReturn("image.jpg");
-//        when(file.getBytes()).thenReturn(new byte[0]);
-//
-//        String result = imageService.saveImage(file);
-//
-//        assertNotNull(result);
-//        assertTrue(result.contains("image.jpg"));
-//    }
-//
-//    @Test
-//    void saveImage_throwsExceptionWhenFileIsEmpty() {
-//        MultipartFile file = mock(MultipartFile.class);
-//        when(file.isEmpty()).thenReturn(true);
-//
-//        ApiException exception = assertThrows(ApiException.class, () -> imageService.saveImage(file));
-//        assertEquals(HttpStatus.BAD_REQUEST, exception.getStatus());
-//        assertEquals("Image file must not be empty", exception.getMessage());
-//    }
 
     @Test
     void getImage_returnsImageSuccessfully() throws IOException {
-        Path filePath = Paths.get("imageDirectory").resolve("image.jpg");
-        Files.createFile(filePath);
+
+        when(fileManager.loadFile("image.jpg")).thenReturn(
+                new UrlResource(Path.of("image.jpg").toUri()));
+
         Resource resource = imageService.getImage("image.jpg");
 
         assertNotNull(resource);
-        assertTrue(resource.exists());
-        Files.delete(filePath);
     }
 
     @Test
-    void getImage_throwsExceptionWhenFileNotFound() {
-        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () -> imageService.getImage("nonexistent.jpg"));
+    void getImage_throwsResourceNotFoundExceptionWhenFileNotFound() throws IOException {
+        when(fileManager.loadFile("nonexistent.jpg")).thenThrow(
+                new ResourceNotFoundException("File", "nonexistent.jpg"));
+
+        var exception = assertThrows(ResourceNotFoundException.class,
+                            () -> imageService.getImage("nonexistent.jpg"));
+
         assertEquals("File not found with name nonexistent.jpg", exception.getMessage());
         assertEquals("nonexistent.jpg", exception.getFieldName());
+    }
+
+    @Test
+    void getImage_throwsApiExceptionWhenResourceNotReadable() throws IOException {
+        when(fileManager.loadFile("nonexistent.jpg")).thenThrow(
+                new ApiException(HttpStatus.INTERNAL_SERVER_ERROR,
+                                "Could not read the file: nonexistent.jpg"));
+
+        var exception = assertThrows(ApiException.class,
+                () -> imageService.getImage("nonexistent.jpg"));
+
+        assertEquals("Could not read the file: nonexistent.jpg", exception.getMessage());
     }
 
     @Test
     void testDeleteImages_ShouldDeleteFiles() {
         List<String> files = List.of("image1.jpg", "image2.jpg");
 
-        when(clothe.getImages()).thenReturn(new ArrayList<>(files));
         when(clotheUtils.getClotheFromDB(1L)).thenReturn(clothe);
+        when(clothe.getImages()).thenReturn(new ArrayList<>(files));
+        doNothing().when(fileManager).deleteFile(anyString());
 
         imageService.deleteImages(1L, files, EMAIL);
 
-        verify(clothe, times(2)).getImages();
+        verify(clothe, times(4)).getImages();
         verify(clotheUtils, times(1)).saveClotheInDB(clothe);
+        verify(fileManager, times(2)).deleteFile(anyString());
     }
 
     @Test
-    void deleteImages_throwsExceptionWhenImageNotBelongToClothe() {
+    void deleteImages_throwsApiExceptionWhenImageNotBelongToClothe() {
         when(clotheUtils.getClotheFromDB(1L)).thenReturn(clothe);
         when(clothe.getImages()).thenReturn(List.of("image1.jpg"));
 
         ApiException exception = assertThrows(
                 ApiException.class, () -> imageService.deleteImages(1L, List.of("image2.jpg"), EMAIL));
 
-        assertEquals("Image image2.jpg doesn't belong to clothe with id: 1", exception.getMessage());
+        assertEquals("Image image2.jpg doesn't belong to clothe with id: 0", exception.getMessage());
         assertEquals(HttpStatus.BAD_REQUEST, exception.getStatus());
     }
 
     @Test
-    void testUploadImages_ShouldSaveImages() throws IOException {
-
-        when(clothe.getImages()).thenReturn(new ArrayList<>());
-        when(file1.getOriginalFilename()).thenReturn("image1.jpg");
-        when(file2.getOriginalFilename()).thenReturn("image2.jpg");
-        when(file1.isEmpty()).thenReturn(false);
-        when(file2.isEmpty()).thenReturn(false);
-        when(file1.getBytes()).thenReturn(new byte[10]);
-        when(file2.getBytes()).thenReturn(new byte[10]);
+    void deleteImages_throwsResourceNotFoundExceptionWhenFileNotFound() {
         when(clotheUtils.getClotheFromDB(1L)).thenReturn(clothe);
+        when(clothe.getImages()).thenReturn(List.of("image2.jpg"));
+        doThrow(new ResourceNotFoundException("File", "image2.jpg"))
+            .when(fileManager).deleteFile("image2.jpg");
+
+        var exception = assertThrows(ResourceNotFoundException.class,
+                    () -> imageService.deleteImages(1L, List.of("image2.jpg"), EMAIL));
+
+        assertEquals("File not found with name image2.jpg", exception.getMessage());
+        assertEquals("image2.jpg", exception.getFieldName());
+    }
+
+    @Test
+    void testUploadImages_ShouldSaveImages() {
+        when(clotheUtils.getClotheFromDB(1L)).thenReturn(clothe);
+        when(clothe.getImages()).thenReturn(new ArrayList<>());
+        when(fileManager.saveFile(file1)).thenReturn("image1.jpg");
 
         imageService.uploadImages(1L, List.of(file1, file2), EMAIL);
 
         assertEquals(2, clothe.getImages().size());
         verify(clotheUtils, times(1)).saveClotheInDB(clothe);
+        verify(fileManager, times(2)).saveFile(any());
     }
 
     @Test
-    void uploadImages_throwsExceptionWhenExceedingImageLimit() {
+    void uploadImages_throwsApiExceptionWhenExceedingImageLimit() {
         when(clotheUtils.getClotheFromDB(1L)).thenReturn(clothe);
         when(clothe.getImages()).thenReturn(List.of(
                 "image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg"));
@@ -148,5 +154,37 @@ public class ImageServiceTest {
 
         assertEquals(HttpStatus.BAD_REQUEST, exception.getStatus());
         assertEquals(Constant.IMAGES_VALIDATION_FAILED, exception.getMessage());
+    }
+
+    @Test
+    void uploadImages_throwsApiExceptionWhenFileIsEmpty() {
+        String message = "Image file must not be empty";
+
+        when(clotheUtils.getClotheFromDB(1L)).thenReturn(clothe);
+        when(clothe.getImages()).thenReturn(new ArrayList<>());
+        when(fileManager.saveFile(file1))
+                .thenThrow(new ApiException(HttpStatus.BAD_REQUEST, message));
+
+        ApiException exception = assertThrows(
+                ApiException.class, () -> imageService.uploadImages(1L, List.of(file1), EMAIL));
+
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getStatus());
+        assertEquals(message, exception.getMessage());
+    }
+
+    @Test
+    void uploadImages_throwsApiExceptionWhenCannotSaveAFile() {
+        String message = "Could not save file";
+
+        when(clotheUtils.getClotheFromDB(1L)).thenReturn(clothe);
+        when(clothe.getImages()).thenReturn(new ArrayList<>());
+        when(fileManager.saveFile(file1))
+                .thenThrow(new ApiException(HttpStatus.INTERNAL_SERVER_ERROR, message));
+
+        ApiException exception = assertThrows(
+                ApiException.class, () -> imageService.uploadImages(1L, List.of(file1), EMAIL));
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, exception.getStatus());
+        assertEquals(message, exception.getMessage());
     }
 }
