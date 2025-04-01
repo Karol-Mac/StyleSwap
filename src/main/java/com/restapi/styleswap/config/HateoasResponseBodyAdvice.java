8268caf5689ec32fc0ec73e5dd42aaa63d297229
Karol Mac <karol200242@gmail.com>
2025-04-01 23:07:43 +0200

    ResponseBodyAdviser and WebConfig added

---
+++ src/main/java/com/restapi/styleswap/config/HateoasResponseBodyAdvice.java
@@ -0,0 +1,60 @@
+package com.restapi.styleswap.config;
+
+import com.restapi.styleswap.payload.ClotheDto;
+import com.restapi.styleswap.payload.ClotheResponse;
+import com.restapi.styleswap.utils.assemblers.ClotheModelAssembler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@Slf4j
+@ControllerAdvice
+public class HateoasResponseBodyAdvice implements ResponseBodyAdvice<Object> {
+
+    private final ClotheModelAssembler assembler;
+
+    @Autowired
+    public HateoasResponseBodyAdvice(ClotheModelAssembler assembler) {
+        this.assembler = assembler;
+    }
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType,
+                                  MediaType selectedContentType,
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request,
+                                  ServerHttpResponse response) {
+
+        String acceptHeader = request.getHeaders().getFirst(HttpHeaders.ACCEPT);
+//        String path = request.getURI().getPath();
+
+        Object actualBody = body;
+        if (body instanceof ResponseEntity<?> responseEntity) {
+            actualBody = responseEntity.getBody();
+        }
+
+        if (acceptHeader != null && acceptHeader.toLowerCase().contains("application/hal+json")) {
+
+            if (actualBody instanceof ClotheDto dto) {
+                return assembler.toModel(dto);
+            } else if (actualBody instanceof ClotheResponse responseDto) {
+
+                return assembler.toCollectionModel(responseDto);
+            }
+        }
+        return body;
+    }
+}
---
+++ src/main/java/com/restapi/styleswap/config/WebConfig.java
@@ -0,0 +1,41 @@
+package com.restapi.styleswap.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer
+                .favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("hal+json", MediaType.parseMediaType("application/hal+json"));
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        List<MediaType> mediaTypes = new ArrayList<>(converter.getSupportedMediaTypes());
+        mediaTypes.add(MediaType.parseMediaType("application/hal+json"));
+        converter.setSupportedMediaTypes(mediaTypes);
+        return converter;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(mappingJackson2HttpMessageConverter());
+    }
+}
